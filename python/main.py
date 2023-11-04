de340c91e676892621ab369e10d16b988528df4a
Paschalis Bizopoulos <pbizopoulos@protonmail.com>
2023-11-04 22:16:34 +0200

    .

--- python/main.py
+++ python/main.py
@@ -16,7 +16,18 @@ def main() -> None:
         if isinstance(node, ast.Name):
             tokens = nltk.word_tokenize(node.id)
             pos_tags = nltk.pos_tag(tokens)
-            if pos_tags and pos_tags[0][1].startswith("N"):
+            if (
+                pos_tags
+                and pos_tags[0][1] == "NN"
+                and (len(pos_tags) == 1 or pos_tags[1][1].startswith(("JJ", "RB")))
+            ):
+                assert True
+            else:
+                raise AssertionError
+        elif isinstance(node, ast.FunctionDef):
+            tokens = nltk.word_tokenize(node.name)
+            pos_tags = nltk.pos_tag(tokens)
+            if pos_tags and (pos_tags[0][1] == "VB" or pos_tags[0][1] == "VBP"):
                 assert True
             else:
                 raise AssertionError
