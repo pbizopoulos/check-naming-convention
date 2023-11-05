36e0c6118d066cb59c0a5bbcdfc45dd32937a618
Paschalis Bizopoulos <pbizopoulos@protonmail.com>
2023-11-05 11:39:13 +0200

    .

--- python/main.py
+++ python/main.py
@@ -1,37 +1,14 @@
-import ast
-from os import environ
+import unittest
 from pathlib import Path
 
-import nltk
+from python.main import naming_convention
 
 
-def main() -> None:
-    environ["NLTK_DATA"] = "tmp/"
-    nltk.download("punkt")
-    nltk.download("averaged_perceptron_tagger")
-    with Path("prm/sample.py").open() as file:
-        code = file.read()
-    tree = ast.parse(code)
-    for node in ast.walk(tree):
-        if isinstance(node, ast.Name):
-            tokens = nltk.word_tokenize(node.id)
-            pos_tags = nltk.pos_tag(tokens)
-            if (
-                pos_tags
-                and pos_tags[0][1] == "NN"
-                and (len(pos_tags) == 1 or pos_tags[1][1].startswith(("JJ", "RB")))
-            ):
-                assert True
-            else:
-                raise AssertionError
-        elif isinstance(node, ast.FunctionDef):
-            tokens = nltk.word_tokenize(node.name)
-            pos_tags = nltk.pos_tag(tokens)
-            if pos_tags and (pos_tags[0][1] == "VB" or pos_tags[0][1] == "VBP"):
-                assert True
-            else:
-                raise AssertionError
+class Tests(unittest.TestCase):
+    def test_naming_convention(self: "Tests") -> None:
+        with Path("prm/sample.py").open(encoding="utf-8") as file:
+            naming_convention(file)
 
 
 if __name__ == "__main__":
-    main()
+    unittest.main()
---
+++ python/python/main.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+import ast
+import sys
+from io import TextIOWrapper
+from os import environ
+from pathlib import Path
+
+import nltk
+
+environ["NLTK_DATA"] = "tmp/"
+nltk.download("punkt")
+nltk.download("averaged_perceptron_tagger")
+
+
+def naming_convention(code_input: str | TextIOWrapper) -> None:
+    if isinstance(code_input, str):
+        with Path(code_input).open() as file:
+            root = ast.parse(file.read())
+    elif isinstance(code_input, TextIOWrapper):
+        root = ast.parse(code_input.read())
+    for node in ast.walk(root):
+        if isinstance(node, ast.Name):
+            tokens = nltk.word_tokenize(node.id)
+            pos_tags = nltk.pos_tag(tokens)
+            if (
+                pos_tags
+                and pos_tags[0][1] == "NN"
+                and (len(pos_tags) == 1 or pos_tags[1][1].startswith(("JJ", "RB")))
+            ):
+                assert True
+            else:
+                raise AssertionError
+        elif isinstance(node, ast.FunctionDef):
+            tokens = nltk.word_tokenize(node.name)
+            pos_tags = nltk.pos_tag(tokens)
+            if pos_tags and (pos_tags[0][1] == "VB" or pos_tags[0][1] == "VBP"):
+                assert True
+            else:
+                raise AssertionError
+
+
+def main() -> None:
+    num_arguments_allowed = 2
+    if len(sys.argv) == num_arguments_allowed:
+        naming_convention(sys.argv[1])
+
+
+if __name__ == "__main__":
+    main()
