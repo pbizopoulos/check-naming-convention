c75d6d840de1999390801f6c4e474e8d3c1c1d27
Paschalis Bizopoulos <pbizop@gmail.com>
2023-11-20 21:40:10 +0200

    .

--- docs/prm/main.py
+++ docs/prm/main.py
@@ -6,7 +6,21 @@ from js import document
 from pyodide.ffi.wrappers import add_event_listener
 
 from main import naming_convention
+from js import fetch
+import nltk
+from pathlib import Path
+import os, sys, io, zipfile
 
+response = await fetch('https://raw.githubusercontent.com/nltk/nltk_data/gh-pages/packages/tokenizers/punkt.zip')
+js_buffer = await response.arrayBuffer()
+py_buffer = js_buffer.to_py()  # this is a memoryview
+stream = py_buffer.tobytes()  # now we have a bytes object
+d = Path("/nltk_data/tokenizers")
+d.mkdir(parents=True, exist_ok=True)
+Path('/nltk_data/tokenizers/punkt.zip').write_bytes(stream)
+zipfile.ZipFile('/nltk_data/tokenizers/punkt.zip').extractall(
+    path='/nltk_data/tokenizers/'
+)
 
 def on_keyup_input_textarea(_: None) -> None:
     document.getElementById("input-textarea").style.height = "1px"
